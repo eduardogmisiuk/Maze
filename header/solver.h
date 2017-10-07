84b0fd00e64f1f48f6f89c94f8a4af5368afc7c5
eduardogmisiuk <eduardogmisiuk@gmail.com>
2017-10-07 20:49:14 -0300

    Created the Solver class.

---
+++ header/solver.h
@@ -0,0 +1,16 @@
+#ifndef _SOLVER_H_
+#define _SOLVER_H_
+
+using namespace std;
+
+class Solver {
+private:
+	
+
+public:
+	Solver ();
+	~Solver ();
+
+};
+
+#endif
---
+++ src/solver.cc
@@ -0,0 +1,5 @@
+Solver::Solver () {
+}
+
+Solver::~Solver (){
+}
