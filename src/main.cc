4d760441dbfde0b39d1a6fb4640038c6ba08baf4
Leonardo <lgomes.leo@gmail.com>
2017-10-08 15:39:02 -0300

    Iteração final da main.

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,49 @@
 #include <iostream>
 #include "maze.h"
 #include "solver.h"
+#include "bruteforcer.h"
+
+#include <cstdio>
 
 int main (int argc, char *argv[]) {
-	Maze *maze = Maze::fromFile("in/5.in");
-	Solver s;
-	s.informedSearch (*maze);
-	maze->print ();
+	// main <maze file> <mode>
+	// <mode> = 0 : A*; 1 : backtracking; 2 : ambos
+	if (argc < 3) {
+		cout << "uso:" << endl;
+		cout << "./" << argv[0] << " <arquivo.in> <modo>" << endl;
+		cout << "<modo> = 0, 1 ou 2, onde 0 = A*, 1 = backtracking, 2 = ambos." << endl;
+		return 1;
+	}
+
+	string filename = argv[1];
+	Maze* maze = Maze::fromFile(filename);
+
+	cout << "Labirinto original:" << endl;
+	maze->print();
+	cout << endl << endl;
+
+	int mode = std::atoi(argv[2]);
+
+	if (mode == 0 || mode == 2) {
+		cout << "A*:" << endl;
+		Solver s;
+		s.informedSearch(*maze);
+		maze->print();
+		cout << endl;
+	}
+
+	if (mode == 2)
+		maze->clear();
+
+	if (mode == 1 || mode == 2) {
+		cout << "Busca em largura (backtracking):" << endl;
+		Bruteforcer b;
+		b.bruteforce(*maze);
+		maze->print();
+		cout << endl;
+	}
+
 	delete maze;
+
 	return 0;
 }
